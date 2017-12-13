f516090cacb477cef28abfd506b02a7ce0a85c4f
Madhu Rakhal Magar <rakhal.madhu@gmail.com>
2017-12-13 11:22:47 +0100

    udated thesarus with new words

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const thesaurus = require("thesaurus");
+const updated_thesaurus = thesaurus.load("./theraus/th_en_US_new.dat");
 const app = express()
 
 app.use(express.static('public'))
 
 app.get('/similarwords', (req, res) => {
 	const queryWord = req.query['word'];
-	const similarWords = thesaurus.find(queryWord)
+	const similarWords = updated_thesaurus.find(queryWord)
 	res.json({data: similarWords })
 });
 
